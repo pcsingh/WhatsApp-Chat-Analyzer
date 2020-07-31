ccf6d10e292ba7c49245ea31bc35d9cfa6b660a8
pcsingh <premchandra.singh.5268@gmail.com>
2020-07-31 18:14:49 +0530

    make it better

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+	[general]\n\
+	email=\"premchandra.singh.5268@gmail.com\"\n\
+	" > ~/.streamlit/credentials.toml
+
+echo "\
 	[server]\n\
 	headless = true\n\
 	port = $PORT\n\
